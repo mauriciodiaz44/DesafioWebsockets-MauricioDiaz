4669f49cd298f0016c3914a9b6b937855af0eaef
mauriciodiaz44 <mauriciodiaz.dev@gmail.com>
2023-07-19 21:11:22 -0300

    first commit on experimental

--- src/app.js
+++ src/app.js
@@ -1,10 +1,25 @@
 import express from "express";
 import productsRouter from "./routes/products.js";
 import cartsRouter from "./routes/carts.js";
+import handlebars from "express-handlebars";
+import __dirname from "./dirname.js";
 
 const app = express();
+
+app.engine("handlebars", handlebars.engine());
+
+app.set("views", `${__dirname}/views`);
+app.set("view engine", "handlebars");
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  if ((req.ip = "127.0.0.1")) {
+  } else {
+    next();
+  }
+});
 
 app.use("/api/products", productsRouter);
 app.use("/api/carts", cartsRouter);
